e40b8193fea41c7beb64f0bf5c55f2de752d1cb7
yakirnisim1 <yakir112233@gmail.com>
2024-03-31 13:45:20 -0700

    my first goland webserver-00

---
+++ webserver.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handleJSON)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleJSON(w http.ResponseWriter, r *http.Request) {
+	msg := "ok"
+	respons, err := json.Marshal(msg)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(respons)
+}

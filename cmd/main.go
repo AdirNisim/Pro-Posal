c0b36f54eef0d2b72c8d08d0f953ef65f1d12f43
adirn <adirnnisim1@gmail.com>
2024-04-04 22:58:05 +0300

    Added Confing.go , Middleware for JSON, Changed the error to Log so we can view the problems and return something is wrong

--- cmd/main.go
+++ cmd/main.go
@@ -4,28 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"webserver/internal/routes"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-
-	err := godotenv.Load("_.env") // This will look for a ".env" file in the current directory
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
 	// Initialize router (for different api routes)
 	router := routes.NewRouter()
 
-	// Read port from env file
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
+	addr := fmt.Sprintf(":%s", AppConfig.Port)
 
 	// Start the server
 	if err := http.ListenAndServe(addr, router); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
-
 }

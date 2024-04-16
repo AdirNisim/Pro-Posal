8fb7e538c75c3a0cd5a4eec699ceca4b858ac4a3
yakirnisim1 <yakir112233@gmail.com>
2024-04-16 16:18:09 -0700

    Implemented pull request suggestions and add missing files and directories

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM golang:1.18-alpine
-
-WORKDIR /app
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,10 +1,19 @@
 package routes
 
 import (
+	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
+	"time"
+	"webserver/models"
+	"webserver/userlib"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func NewRouter() http.Handler {
@@ -13,7 +22,8 @@ func NewRouter() http.Handler {
 
 	router.HandleFunc("/", handleJSON).Methods("GET")
 	router.HandleFunc("/api/templates/{id}", handleRouteWithVariable).Methods("GET")
-	router.HandleFunc("/api/user/{name}", handleUserRequest).Methods("GET")
+	router.HandleFunc("/v1/addUser", handleUserPostRequest).Methods("POST")
+	router.HandleFunc("/v1/getUser", handleUserGetRequest).Methods("POST")
 	return router
 }
 
@@ -48,19 +58,111 @@ func handleRouteWithVariable(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
-func handleUserRequest(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	name := vars["user"]
+// handleUserPostRequest handles the POST request for creating a new user.
+// returns a 200 status code if successful.
+// Note: The handling of the 'invitedBy' input is pending implementation.
 
-	msg := map[string]string{"status": "ok", "id": name}
-	response, err := json.Marshal(msg)
-
+func handleUserPostRequest(w http.ResponseWriter, r *http.Request) {
+	var userInput userlib.UserInput
+	err := json.NewDecoder(r.Body).Decode(&userInput)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Error decoding JSON", http.StatusBadRequest)
+		return
+	}
+
+	passHash, err := userlib.HashPassword(userInput.Password)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+
+	if !userlib.IsValidEmail(userInput.Email) {
+		http.Error(w, "Not a valid Email", http.StatusInternalServerError)
+		return
+	}
+
+	// Need to know how to handle invitedBy input.
+	newUser := models.User{
+		ID:           uuid.New().String(),
+		FirstName:    userInput.FirstName,
+		LastName:     userInput.LastName,
+		Phone:        userInput.Phone,
+		Email:        userInput.Email,
+		EmailHash:    userlib.HashEmail(userInput.Email),
+		PasswordHash: passHash,
+		InvitedBy:    null.String{},
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+	}
+
+	db, err := sql.Open("postgres", "user=admin password=Aa123456 dbname=pro-posal host=localhost sslmode=disable")
+	if err != nil {
+		log.Fatalf("Could not connect to the database! %v", err)
+	}
+	defer db.Close() // Will exec in the end of main
+
+	ctx := r.Context()
+	err = newUser.Insert(ctx, db, boil.Infer())
+	if err != nil {
+		log.Fatalf("Error inserting user: %v", err)
+		http.Error(w, "User is already in the system. Please log-in!", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	log.Printf("User created successfully")
+}
+
+// handleUserGetRequest handles the GET request for user information retrieval.
+// If successful, it returns the user's information in JSON format with status code 200 (OK).
+// Otherwise, it sends appropriate error responses
+func handleUserGetRequest(w http.ResponseWriter, r *http.Request) {
+	var userInput userlib.LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&userInput)
+	if err != nil {
+		http.Error(w, "Error decoding JSON", http.StatusBadRequest)
+		return
+	}
+
+	user, err := userlib.GetUser(userInput.Email, userInput.Password, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	jsonData, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, "Error marshaling JSON", http.StatusInternalServerError)
+		log.Printf("Error marshaling JSON")
+		return
+	}
+
+	log.Printf("User was successfully sent.")
+	w.Write(jsonData)
+}
+
+func handleUserUpdatePassword(w http.ResponseWriter, r *http.Request) {
+	var userInput userlib.LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&userInput)
+	if err != nil {
+		http.Error(w, "Error decoding JSON", http.StatusBadRequest)
+		return
+	}
+
+	user, err := userlib.GetUser(userInput.Email, userInput.Password, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(response)
+	jsonData, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, "Error marshaling JSON", http.StatusInternalServerError)
+		log.Printf("Error marshaling JSON")
+		return
+	}
 
+	log.Printf("User was successfully sent.")
+	w.Write(jsonData)
 }
